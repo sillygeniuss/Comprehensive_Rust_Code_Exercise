bfd7dbab5668ab8cd6000a1dc5ec233d03c49586
sillygenius <sillygenius@outlook.com>
2024-01-08 11:54:14 +0800

    content 2: control flow basics

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,8 @@ fn main() {
 
     let n = 20;
     println!("fib({n}): {}", fib(n));
+
+    condition_test();
 }
 
 fn fib(n: u32) -> u32 {
@@ -31,5 +33,46 @@ fn fib(n: u32) -> u32 {
         return 1;
     } else {
         return fib(n - 1) + fib(n - 2);
+    }
+}
+
+fn condition_test() {
+    let x = 20;
+
+    // difference between expression and statement
+    let size = if x < 20 { "small" } else { "large" };
+    // let size = if x < 20 { "small"; } else { "large" };
+
+    println!("size: {size}");
+
+    for x in 1..5 {
+        println!("x: {}", x);
+    }
+
+    // use `break` for expression
+    let (mut a, mut b) = (100, 53);
+    let result = loop {
+        if a == b {
+            break a;
+        }
+
+        if a < b {
+            b -= a;
+        } else {
+            a -= b;
+        }
+    };
+    println!("result: {}", result);
+
+    // use `label` break nested loop
+    'flag: for x in 1..5 {
+        println!("x: {}", x);
+        let mut i = 0;
+        while i < x {
+            i += 1;
+            if i == 3 {
+                break 'flag;
+            }
+        }
     }
 }
