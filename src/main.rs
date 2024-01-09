fc031e88e9475915903fb1cb2ac1f55e6c166730
sillygenius <sillygenius@outlook.com>
2024-01-09 14:30:13 +0800

    stage: arrays & tuples

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,8 @@ fn main() {
 
     // String | &str
     // default immutable
-    let plantform = "x86_64";
-    println!("plantform: {plantform}");
+    let platform = "x86_64";
+    println!("platform: {platform}");
 
     // explicit mutable string
     let mut system = "Linux";
@@ -25,7 +25,8 @@ fn main() {
     let n = 20;
     println!("fib({n}): {}", fib(n));
 
-    condition_test();
+    stage_condition();
+    stage_tuples_arrays();
 }
 
 fn fib(n: u32) -> u32 {
@@ -36,7 +37,7 @@ fn fib(n: u32) -> u32 {
     }
 }
 
-fn condition_test() {
+fn stage_condition() {
     let x = 20;
 
     // difference between expression and statement
@@ -73,6 +74,32 @@ fn condition_test() {
             if i == 3 {
                 break 'flag;
             }
+        }
+    }
+}
+
+fn stage_tuples_arrays() {
+    // array
+    let mut a: [i32; 10] = [42;10];
+    println!("index 5 is {}", a[4]);
+    // println!("out-of-bounds value is {}", a[10]);
+    println!("complete array content is {a:?}");
+    println!("complete array pretty content is {a:#?}");
+
+    a[4] = 10;
+    println!("change value of index 5: {}", a[4]);
+
+    // tuples
+    let t: (i32, f32, bool) = (10, 3.14, true);
+    println!("index 1 of tuples is: {}", t.1);
+
+    let arr1 = [1, 2, 3, 4, 5];
+    match arr1 {
+        [a@.., x, b] if x == 4 => {
+            println!("prefix arr{:#?}", a);
+        }
+        _ => {
+            println!("not match");
         }
     }
 }
